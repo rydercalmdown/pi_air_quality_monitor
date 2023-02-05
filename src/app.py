b1a8def47ccad31273ed8068ed2948a9cb0b55a8
Jan Sviland <jan.sviland@systek.no>
2023-02-05 13:59:40 +0100

    set correct ip

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 aqm = AirQualityMonitor()
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=aqm.save_measurement_to_redis, trigger="interval", seconds=60)
+scheduler.add_job(func=aqm.save_measurement_to_redis, trigger="interval", seconds=10)
 scheduler.start()
 atexit.register(lambda: scheduler.shutdown())
 
