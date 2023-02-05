d9a7eccfb4a67a9e7229c0bf235afeb43fe4e963
Jan Sviland <jan.sviland@systek.no>
2023-02-05 13:45:47 +0100

    get measurement

--- scripts/getMeasurement.py
+++ scripts/getMeasurement.py
@@ -2,5 +2,10 @@ from sds011 import SDS011
 
 port = "/dev/ttyUSB0"
 sds = SDS011(port=port)
+sds.set_working_period(rate=5)
+meas = sds.read_measurement()
 
-print(sds)
+print(meas)
+
+
+# print(sds)
