283724092839b4b3d23dadf488e834c8c355a659
Stefano Chittaro <45672541+nevarsin@users.noreply.github.com>
2024-07-30 21:45:05 +0200

    Switch to sds011lib library (#20)
    Co-authored-by: Stefano Chittaro <ext-Stefano.Chittaro@estos.de>

--- src/AirQualityMonitor.py
+++ src/AirQualityMonitor.py
@@ -5,6 +5,7 @@ import datetime
 import serial
 import redis
 import aqi
+from sds011lib import SDS011QueryReader
 
 redis_client = redis.StrictRedis(host=os.environ.get('REDIS_HOST'), port=6379, db=0)
 
@@ -12,15 +13,15 @@ redis_client = redis.StrictRedis(host=os.environ.get('REDIS_HOST'), port=6379, d
 class AirQualityMonitor():
 
     def __init__(self):
-        self.ser = serial.Serial('/dev/ttyUSB0')
+        self.ser = SDS011QueryReader('/dev/ttyUSB0')
 
     def get_measurement(self):
         self.data = []
         for index in range(0,10):
-            datum = self.ser.read()
+            datum = self.ser.query()
             self.data.append(datum)
-        self.pmtwo = int.from_bytes(b''.join(self.data[2:4]), byteorder='little') / 10
-        self.pmten = int.from_bytes(b''.join(self.data[4:6]), byteorder='little') / 10
+        self.pmtwo = datum.pm25
+        self.pmten = datum.pm10
         myaqi = aqi.to_aqi([(aqi.POLLUTANT_PM25, str(self.pmtwo)),
                             (aqi.POLLUTANT_PM10, str(self.pmten))])
         self.aqi = float(myaqi)
@@ -31,6 +32,7 @@ class AirQualityMonitor():
             "pm10": self.pmten,
             "aqi": self.aqi,
         }
+        
         return {
             'time': int(time.time()),
             'measurement': self.meas
